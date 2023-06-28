b2317af61407c236ebea7a6a645ff35cc93e59ef
mohsinabbasvisionet <mohsin.abbas@visionet.com>
2023-06-28 18:40:11 +0500

    husky config added

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../userModel');
-//const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 
 //signUp User
